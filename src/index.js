7d9bb42e02b9156a7e3a0829490ac6887657e358
yaejin9503 <jimmy0152@naver.com>
2022-01-10 18:02:36 +0900

    06_Node.js 핵심 개념 정리_03_Node.js 컨벤션

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 // @ts-check
 
 //require
-const { path, paths, filename } = module;
-
-console.log({
-  path,
-  paths,
-  filename,
-});
-
+const { log } = console;
+const fs = require('fs');
+// log(require('./module'));
 // CommonJS = require
 // ECMA module = import export
+
+//callback-style
+fs.readFile('./module.js', (err, result) => {
+  if (err) log(err);
+  else log(result);
+});
+
+//error - handling 시 try catch로 에러처리
+// sync-style
+try {
+  const result = fs.readFileSync('./module.js', 'utf-8');
+  log('동기적', result);
+} catch (e) {
+  console.error(e);
+}
